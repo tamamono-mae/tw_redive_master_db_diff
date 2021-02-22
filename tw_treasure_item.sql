2031133bfd2a6b0ce22b8d4bfa827371136b9a65
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-02-22 20:07:51 +0800

    00017003 - new event - new quest area 39.拉圖斯斷崖‧北部, 39.拉圖斯斷崖‧北部 (Hard), 24.雷蒂烏斯群峰‧西麓 - max level to 178

--- tw_treasure_item.sql
+++ tw_treasure_item.sql
@@ -3,3 +3,7 @@ INSERT INTO `tw_treasure_item` VALUES (/*id*/1, /*item_id*/97501, /*type*/1, /*p
 INSERT INTO `tw_treasure_item` VALUES (/*id*/2, /*item_id*/97502, /*type*/2, /*point*/3, /*message_id*/97502);
 INSERT INTO `tw_treasure_item` VALUES (/*id*/3, /*item_id*/97503, /*type*/1, /*point*/1, /*message_id*/97503);
 INSERT INTO `tw_treasure_item` VALUES (/*id*/4, /*item_id*/97504, /*type*/1, /*point*/1, /*message_id*/97504);
+INSERT INTO `tw_treasure_item` VALUES (/*id*/5, /*item_id*/97505, /*type*/1, /*point*/1, /*message_id*/97505);
+INSERT INTO `tw_treasure_item` VALUES (/*id*/6, /*item_id*/97506, /*type*/2, /*point*/3, /*message_id*/97506);
+INSERT INTO `tw_treasure_item` VALUES (/*id*/7, /*item_id*/97507, /*type*/2, /*point*/3, /*message_id*/97507);
+INSERT INTO `tw_treasure_item` VALUES (/*id*/8, /*item_id*/97508, /*type*/2, /*point*/3, /*message_id*/97508);
