4c049d18e0c4eea286dd33c82d08c8b012320021
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-02-22 18:51:45 +0800

    00008007 - new clan battle

--- campaign_schedule.sql
+++ campaign_schedule.sql
@@ -136,3 +136,5 @@ INSERT INTO `campaign_schedule` VALUES (/*id*/134, /*campaign_category*/41, /*va
 INSERT INTO `campaign_schedule` VALUES (/*id*/135, /*campaign_category*/251, /*value*/1500.0, /*system_id*/7004, /*icon_image*/80, /*start_time*/"2019/11/17 16:00:00", /*end_time*/"2019/11/24 15:59:59");
 INSERT INTO `campaign_schedule` VALUES (/*id*/136, /*campaign_category*/252, /*value*/1500.0, /*system_id*/7005, /*icon_image*/80, /*start_time*/"2019/11/17 16:00:00", /*end_time*/"2019/11/24 15:59:59");
 INSERT INTO `campaign_schedule` VALUES (/*id*/137, /*campaign_category*/37, /*value*/2000.0, /*system_id*/109, /*icon_image*/30, /*start_time*/"2019/11/20 05:00:00", /*end_time*/"2019/11/26 04:59:59");
+INSERT INTO `campaign_schedule` VALUES (/*id*/138, /*campaign_category*/32, /*value*/2000.0, /*system_id*/102, /*icon_image*/30, /*start_time*/"2019/11/26 05:00:00", /*end_time*/"2019/12/01 04:59:59");
+INSERT INTO `campaign_schedule` VALUES (/*id*/139, /*campaign_category*/42, /*value*/2000.0, /*system_id*/102, /*icon_image*/40, /*start_time*/"2019/11/26 05:00:00", /*end_time*/"2019/12/01 04:59:59");
