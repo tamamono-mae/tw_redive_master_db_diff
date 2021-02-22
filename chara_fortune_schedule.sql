45f1f5a2e2584a292eeab78e60c824789538e3ce
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-02-22 19:13:20 +0800

    00012001

--- chara_fortune_schedule.sql
+++ chara_fortune_schedule.sql
@@ -1,3 +1,4 @@
 CREATE TABLE 'chara_fortune_schedule' ('fortune_id' INTEGER NOT NULL, 'name' TEXT NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('fortune_id'));
-INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10000, /*name*/"1st Anniversary 特殊登入獎勵", /*start_time*/"2019/08/01 05:00:00", /*end_time*/"2019/8/15 04:59:00");
+INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10000, /*name*/"1st Anniversary 特殊登入獎勵", /*start_time*/"2019/08/01 05:00:00", /*end_time*/"2019/08/15 04:59:00");
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10002, /*name*/"1.5週年登入禮", /*start_time*/"2020/01/21 05:00:00", /*end_time*/"2020/02/07 04:59:59");
+INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10003, /*name*/"2nd Anniversary 特殊登入獎勵", /*start_time*/"2020/06/22 05:00:00", /*end_time*/"2020/07/08 04:59:59");
