6b383cf360bc6b4e43d80b80dd7e7fa08f5db2be
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-02-22 18:56:33 +0800

    00009015 - 2 new table: tw_treasure_item, tw_treasure_item_group

---
+++ tw_treasure_item.sql
@@ -0,0 +1,3 @@
+CREATE TABLE 'tw_treasure_item' ('id' INTEGER NOT NULL, 'item_id' INTEGER NOT NULL, 'type' INTEGER NOT NULL, 'point' INTEGER NOT NULL, 'message_id' INTEGER NOT NULL, PRIMARY KEY('id'));
+INSERT INTO `tw_treasure_item` VALUES (/*id*/1, /*item_id*/97501, /*type*/1, /*point*/1, /*message_id*/97501);
+INSERT INTO `tw_treasure_item` VALUES (/*id*/2, /*item_id*/97502, /*type*/2, /*point*/3, /*message_id*/97502);
