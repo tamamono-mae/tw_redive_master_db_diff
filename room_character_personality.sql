5d9c2a5f7582d6261772d7e3bcf7c3b8c2b31ee9
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-02-22 19:48:16 +0800

    00016000 - 4 new table: +manifest_animation, growth_parameter, growth_restriction_unit, room_item_get_announcement

--- room_character_personality.sql
+++ room_character_personality.sql
@@ -122,6 +122,11 @@ INSERT INTO `room_character_personality` VALUES (/*character_id*/1134, /*persona
 INSERT INTO `room_character_personality` VALUES (/*character_id*/1135, /*personality_id*/4);
 INSERT INTO `room_character_personality` VALUES (/*character_id*/1136, /*personality_id*/1);
 INSERT INTO `room_character_personality` VALUES (/*character_id*/1137, /*personality_id*/2);
+INSERT INTO `room_character_personality` VALUES (/*character_id*/1138, /*personality_id*/1);
+INSERT INTO `room_character_personality` VALUES (/*character_id*/1139, /*personality_id*/3);
+INSERT INTO `room_character_personality` VALUES (/*character_id*/1140, /*personality_id*/4);
+INSERT INTO `room_character_personality` VALUES (/*character_id*/1141, /*personality_id*/3);
+INSERT INTO `room_character_personality` VALUES (/*character_id*/1142, /*personality_id*/1);
 INSERT INTO `room_character_personality` VALUES (/*character_id*/1802, /*personality_id*/1);
 INSERT INTO `room_character_personality` VALUES (/*character_id*/1804, /*personality_id*/3);
 INSERT INTO `room_character_personality` VALUES (/*character_id*/1805, /*personality_id*/1);
