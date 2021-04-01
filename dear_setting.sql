4e54ddac7f8fd3d02214b53193aa5e437b3d5c6c
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-04-01 09:30:30 +0800

    00017111 - new unit: ★★★秋乃（聖誕節）, 咲戀（聖誕節）, ★優花梨（聖誕節）

--- dear_setting.sql
+++ dear_setting.sql
@@ -12,4 +12,5 @@ INSERT INTO `dear_setting` VALUES (/*event_id*/10049, /*system_name*/"信賴度"
 INSERT INTO `dear_setting` VALUES (/*event_id*/10050, /*system_name*/"信賴度", /*tutorial_quest_id*/10050103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6050106);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10051, /*system_name*/"信賴度", /*tutorial_quest_id*/10051103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6027101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10052, /*system_name*/"信賴度", /*tutorial_quest_id*/10052103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6052101);
+INSERT INTO `dear_setting` VALUES (/*event_id*/10054, /*system_name*/"信賴度", /*tutorial_quest_id*/10054103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6054101);
 INSERT INTO `dear_setting` VALUES (/*event_id*/10055, /*system_name*/"信賴度", /*tutorial_quest_id*/10055103, /*tutorial_chara_index*/1, /*tutorial_story_id*/6029201);
