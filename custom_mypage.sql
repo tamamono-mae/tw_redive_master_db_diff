1f81e72644b8d350c24840087fcc1a75740c7881
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-08-02 15:30:59 +0800

    00020012 - new unit: ★★★克蘿依（聖學祭）, 優妮（聖學祭）

--- custom_mypage.sql
+++ custom_mypage.sql
@@ -1,2 +1,5 @@
 CREATE TABLE 'custom_mypage' ('still_id' INTEGER NOT NULL, 'group_id' INTEGER NOT NULL, 'still_group_id' INTEGER NOT NULL, 'still_name' TEXT NOT NULL, 'vertical_still_flg' INTEGER NOT NULL, 'scroll_direction' INTEGER NOT NULL, PRIMARY KEY('still_id'));
+INSERT INTO `custom_mypage` VALUES (/*still_id*/9000092, /*group_id*/20001, /*still_group_id*/0, /*still_name*/"3周年紀念自訂主頁面1", /*vertical_still_flg*/0, /*scroll_direction*/0);
+INSERT INTO `custom_mypage` VALUES (/*still_id*/9000093, /*group_id*/20001, /*still_group_id*/0, /*still_name*/"3周年紀念自訂主頁面2", /*vertical_still_flg*/0, /*scroll_direction*/0);
+INSERT INTO `custom_mypage` VALUES (/*still_id*/9000094, /*group_id*/20001, /*still_group_id*/0, /*still_name*/"3周年紀念自訂主頁面3", /*vertical_still_flg*/0, /*scroll_direction*/0);
 CREATE INDEX 'custom_mypage_0_still_group_id' on 'custom_mypage'('still_group_id');
--- custom_mypage_group.sql
+++ custom_mypage_group.sql
@@ -1,1 +1,2 @@
 CREATE TABLE 'custom_mypage_group' ('group_id' INTEGER NOT NULL, 'group_name' TEXT NOT NULL, PRIMARY KEY('group_id'));
+INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20001, /*group_name*/"公主連結！Re:Dive 3周年紀念");
