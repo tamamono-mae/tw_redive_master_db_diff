cbec0b0c35cbacadbec71816fe2778ddfd410a25
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-10-22 23:18:48 +0800

    00030002 - new gacha 「璃乃（奇幻）」「祈梨」的登場

--- gacha_exchange_lineup.sql
+++ gacha_exchange_lineup.sql
@@ -237,4 +237,6 @@ INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/241, /*exchange_id*/131, /*uni
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/242, /*exchange_id*/131, /*unit_id*/112801, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/09/12 16:00:00", /*end_time*/"2021/09/22 15:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/243, /*exchange_id*/133, /*unit_id*/116601, /*rarity*/3, /*gacha_bonus_id*/1047, /*start_time*/"2021/09/22 16:00:00", /*end_time*/"2021/10/02 15:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/244, /*exchange_id*/134, /*unit_id*/116701, /*rarity*/3, /*gacha_bonus_id*/1048, /*start_time*/"2021/10/02 16:00:00", /*end_time*/"2021/10/11 15:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/245, /*exchange_id*/135, /*unit_id*/112901, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/10/11 16:00:00", /*end_time*/"2021/10/18 15:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/246, /*exchange_id*/135, /*unit_id*/106601, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/10/11 16:00:00", /*end_time*/"2021/10/18 15:59:59");
 CREATE INDEX 'gacha_exchange_lineup_0_exchange_id' on 'gacha_exchange_lineup'('exchange_id');
