ed68d7f0a7561bc5186399bcc95ac523b96fbaa1
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2021-11-24 18:10:48 +0800

    00030025 - new gacha 限定角色復刻！登場

--- gacha_exchange_lineup.sql
+++ gacha_exchange_lineup.sql
@@ -261,4 +261,6 @@ INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/265, /*exchange_id*/143, /*uni
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/266, /*exchange_id*/143, /*unit_id*/107901, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/11/16 16:00:00", /*end_time*/"2021/11/20 15:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/267, /*exchange_id*/144, /*unit_id*/117101, /*rarity*/3, /*gacha_bonus_id*/1052, /*start_time*/"2021/11/16 16:00:00", /*end_time*/"2021/11/25 15:59:59");
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/268, /*exchange_id*/145, /*unit_id*/110401, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/11/20 16:00:00", /*end_time*/"2021/11/25 15:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/269, /*exchange_id*/146, /*unit_id*/111801, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/11/25 16:00:00", /*end_time*/"2021/12/02 15:59:59");
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/270, /*exchange_id*/146, /*unit_id*/115001, /*rarity*/3, /*gacha_bonus_id*/0, /*start_time*/"2021/11/25 16:00:00", /*end_time*/"2021/12/02 15:59:59");
 CREATE INDEX 'gacha_exchange_lineup_0_exchange_id' on 'gacha_exchange_lineup'('exchange_id');
